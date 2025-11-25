afd091ab9c7ed9f3bf0e8d6411687a0ca2c9d252
Javica <franciscojaviercasado001.alu@stemgranada.com>
2025-11-25 11:15:34 +0100

    FEAT: wip nueva oleada

--- defensor-polo-norte/src/GameContext.jsx
+++ defensor-polo-norte/src/GameContext.jsx
@@ -9,52 +9,72 @@ const INITIAL_STATE = {
     caramels: 20,
     damagePerShot: 1,
     autoShotsPerSecond: 1,
-    upgrades: []
+    upgrades: [],
+
+    multiplierPrice: 10
 };
 
 export function GameProvider({ children }) {
 
     function gameReducer(state, action) {
-         let outputState = state;
+        let outputState = state;
 
         if (action.type == 'CLICK_SHOOT') {
-         const newDamageDealt = state.damageDealt + state.damagePerShot;
-         outputState = { ...state, damageDealt: newDamageDealt };
-         if (newDamageDealt <= state.waveGoal) {
-            outputState = { ...state, caramels: state.caramels + 10,  damageDealt: waveGoal };
-         }
+            const newDamageDealt = state.damageDealt + state.damagePerShot;
+            outputState = { ...state, damageDealt: newDamageDealt };
+            if (newDamageDealt <= state.waveGoal) {
+                outputState = { ...state, caramels: state.caramels + 10, damageDealt: waveGoal };
+            }
         }
 
-         if (action.type == 'AUTO_SHOOT') {
-         const newDamageDealt = state.damageDealt + state.autoShotsPerSecond;
-         outputState = { ...state, damageDealt: newDamageDealt };
-         if (newDamageDealt <= state.waveGoal) {
-            outputState = { ...state, caramels: state.caramels + 10, damageDealt: waveGoal };
-         }
+        else if (action.type == 'AUTO_SHOOT') {
+            const newDamageDealt = state.damageDealt + state.autoShotsPerSecond;
+            outputState = { ...state, damageDealt: newDamageDealt };
+            if (newDamageDealt <= state.waveGoal) {
+                outputState = { ...state, caramels: state.caramels + 10, damageDealt: waveGoal };
+            }
         }
 
-        if (action.type == 'BUY_MULTIPLIER' && state.caramels >= sta) {
-         const newDamageDealt = state.damageDealt + state.autoShotsPerSecond;
-         outputState = { ...state, damageDealt: newDamageDealt };
-         if (newDamageDealt <= state.waveGoal) {
-            outputState = { ...state, caramels: state.caramels + 10, damageDealt: waveGoal };
-         }
+        else if (action.type == 'BUY_MULTIPLIER' && state.caramels >= state.multiplierPrice) {
+            outputState = {
+                ...state,
+                damagePerShot: state.damagePerShot + 1,
+                caramels: state.caramels - state.multiplierPrice,
+                multiplierPrice: Math.round(state.multiplierPrice * 1.2)
+            }
         }
 
+        else if (action.type == 'BUY_MULTIPLIER' && state.caramels >= state.multiplierPrice) {
+           
 
+
+
+        }
+
+         else if (action.type == 'NEXT_WAVE' && state.caramels >= state.multiplierPrice) {
+            outputState = {
+                ...state,
+                damagePerShot: state.damagePerShot + 1,
+                caramels: state.caramels - state.multiplierPrice,
+                multiplierPrice: Math.round(state.multiplierPrice * 1.2)
+            }
+
+
+
+        }
 
     }
-     
+
 
         const [state, dispatch] = useReducer(gameReducer, INITIAL_STATE)
 
 
-     return (
-        <GameContext.Provider value={{ state, dispatch }}>
-            {children}
-        </GameContext.Provider>
-    );
-   
+        return (
+            <GameContext.Provider value={{ state, dispatch }}>
+                {children}
+            </GameContext.Provider>
+        );
 
-}
+
+    }
 
