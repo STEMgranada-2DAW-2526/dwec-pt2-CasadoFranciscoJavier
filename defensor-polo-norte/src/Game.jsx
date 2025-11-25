527b5c1bf81519c73001fea9db98bd10bbb2c2ab
Javica <franciscojaviercasado001.alu@stemgranada.com>
2025-11-25 11:45:57 +0100

    feat: wip panel info game.jsx

--- defensor-polo-norte/src/Game.jsx
+++ defensor-polo-norte/src/Game.jsx
@@ -1,6 +1,9 @@
 import { useContext } from 'react';
 import { GameContext } from './GameContext';
 
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function Game() {
         const { state, dispatch } = useContext(GameContext);
@@ -10,7 +13,16 @@ export default function Game() {
 
     <div className='container'>
         <div className='row justify-content-center'>
-          <h1 className='col-12 '>{Math.round(state.cookies)} 🍪</h1>
+                <h3 className='col-12'>Daño de oleada:</h3>
+                <info>{state.damageDealt}/{state.waveGoal}</info>
+            </div>
+            <div className='row justify-content-center'>
+                <h2 className='col-12'>Mejoras</h2>
+            </div>
+            <div className='row justify-content-center'>
+                <h2 className='col-12'>Mejoras</h2>
+            </div>
+        <div className='row justify-content-center'>
           <button className='col-5 btn btn-transparent' onClick={() => dispatch({ type: 'CLICK_SHOOT' })}>
             <img className='img-fluid' src={state.torre} />
           </button>
